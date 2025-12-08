af03ecfae96924643709d3f74be2965159ca10d7
Wilbur Omae <wilburomae@gmail.com>
2025-12-08 20:26:33 +0300

    Add @Transactional annotation

--- src/main/java/com/wilburomae/pezeshalms/common/services/impl/GenericFetchService.java
+++ src/main/java/com/wilburomae/pezeshalms/common/services/impl/GenericFetchService.java
@@ -3,6 +3,7 @@ package com.wilburomae.pezeshalms.common.services.impl;
 import com.wilburomae.pezeshalms.common.dtos.Response;
 import com.wilburomae.pezeshalms.common.services.FetchService;
 import com.wilburomae.pezeshalms.common.utilities.StringUtilities;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,7 @@ import static org.springframework.http.HttpStatus.OK;
 @Service
 public class GenericFetchService implements FetchService {
 
+    @Transactional
     @Override
     public <T, U> Response<List<U>> fetch(JpaRepository<T, Long> repository, Function<T, U> mapper, String nameSingular) {
         List<U> fetched = repository.findAll().stream().map(mapper).toList();
@@ -22,6 +24,7 @@ public class GenericFetchService implements FetchService {
         return new Response<>(OK, "Fetched %d %s".formatted(count, (count == 1 ? nameSingular : StringUtilities.toPlural(nameSingular))), fetched);
     }
 
+    @Transactional
     @Override
     public <T, U> Response<U> fetchById(JpaRepository<T, Long> repository, long id, Function<T, U> mapper, String nameSingular) {
         return repository.findById(id)
