3ffb50d1e3f33a9e9a2275a7e114f6231a88acbb
Wilbur Omae <wilburomae@gmail.com>
2025-12-08 15:48:06 +0300

    Add parent type fields to account type response

--- src/main/java/com/wilburomae/pezeshalms/accounts/dtos/AccountType.java
+++ src/main/java/com/wilburomae/pezeshalms/accounts/dtos/AccountType.java
@@ -2,9 +2,15 @@ package com.wilburomae.pezeshalms.accounts.dtos;
 
 import com.wilburomae.pezeshalms.accounts.data.entities.AccountTypeEntity;
 
-public record AccountType(long id, String name, String description) {
+public record AccountType(long id, String name, String description, Long parentTypeId, String parentTypeName,
+                          String parentTypeDescription) {
 
     public static AccountType from(AccountTypeEntity entity) {
-        return new AccountType(entity.getId(), entity.getName(), entity.getDescription());
+        AccountTypeEntity parent = entity.getParentAccountType();
+        if (parent == null) {
+            return new AccountType(entity.getId(), entity.getName(), entity.getDescription(), null, null, null);
+        } else {
+            return new AccountType(entity.getId(), entity.getName(), entity.getDescription(), parent.getId(), parent.getName(), parent.getDescription());
+        }
     }
 }
